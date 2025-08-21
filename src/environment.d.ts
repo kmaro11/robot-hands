64d30f3228bdbf9169993ecda96de201cf1e3133
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-08-21 19:03:15 +0300

    small fix

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,8 +3,11 @@ declare global {
     interface ProcessEnv {
       PAYLOAD_SECRET: string
       DATABASE_URI: string
+      DATABASE_SSL?: 'true' | 'false'
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
+      PREVIEW_SECRET?: string
+      CRON_SECRET?: string
     }
   }
 }
