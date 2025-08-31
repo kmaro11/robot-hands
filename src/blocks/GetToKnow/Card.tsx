c1cea5a0b62a3305288587b54c0e6833c0394814
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-08-31 18:27:02 +0300

    new block

---
+++ src/blocks/GetToKnow/Card.tsx
@@ -0,0 +1,21 @@
+import { Media } from '@/components/Media'
+
+export const Card: React.FC<{
+  title: string
+  subtitle: string
+  media: any
+}> = ({ title, subtitle, media }) => {
+  return (
+    <div className="rounded-[10px] relative h-[379px] md:h-[491px] flex flex-col justify-end">
+      <Media
+        resource={media}
+        imgClassName="absolute inset-0 rounded-[10px] h-full w-full object-cover object-center"
+      />
+      <div className="absolute inset-0 rounded-[10px] pointer-events-none" />
+      <div className="p-5 md:p-7 relative z-10 h-2/5">
+        <h3 className="text-primary text-20 md:text-25 font-bold mb-2">{title}</h3>
+        <p className="text-primary text-12 md:text-15">{subtitle}</p>
+      </div>
+    </div>
+  )
+}
--- src/blocks/GetToKnow/Component.tsx
+++ src/blocks/GetToKnow/Component.tsx
@@ -1,8 +1,56 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Card } from './Card'
+import useEmblaCarousel from 'embla-carousel-react'
 export const GetToKnow: React.FC<{
   title?: string
   subtitle?: string
   cards?: any[]
   link?: any
 }> = ({ title, subtitle, cards, link }) => {
-  return <div>GetToKnow</div>
+  const [ref] = useEmblaCarousel({
+    loop: true,
+    align: 'center',
+    containScroll: 'trimSnaps',
+    duration: 20,
+  })
+
+  const textButton = (link: any) => {
+    if (!link || !link?.label || !link?.linkType) return
+    return (
+      <Button
+        href={link?.linkType === 'custom' ? link?.url : link.page?.slug}
+        linkType={link?.linkType}
+        variant="primary"
+        size="normal"
+        className="mt-[30px] md:mt-11 font-bold mx-auto w-full max-w-full md:max-w-max"
+      >
+        {link?.label}
+      </Button>
+    )
+  }
+
+  return (
+    <div className="w-full pb-[135px] md:pb-[157px]">
+      <div className="max-w-[580px] w-full mx-auto mb-12 md:mb-20 px-8">
+        <h1 className="text-primary text-30 md:text-40 font-bold text-center mb-4 md:mb-8">
+          {title}
+        </h1>
+        <p className="text-primary text-15 md:text-20 text-center">{subtitle}</p>
+      </div>
+      <div className="overflow-hidden px-8" ref={ref}>
+        <div className="flex -ml-6 will-change-transform">
+          {cards?.map((card) => (
+            <div
+              key={card.id}
+              className="pl-4 md:pl-5 shrink-0 min-w-0 basis-[80%] sm:basis-[60%] md:basis-[45%] lg:basis-1/4 2xl:basis-1/6"
+            >
+              <Card {...card} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="max-w-[580px] w-full mx-auto px-8">{link && textButton(link)}</div>
+    </div>
+  )
 }
