3626b95b75f676622ae18d589e74de08cf494cd0
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-08-31 14:28:17 +0300

    fix

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -17,6 +17,40 @@ export const Media: CollectionConfig = {
     update: authenticated,
   },
   upload: {
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 300,
+      },
+      {
+        name: 'square',
+        width: 500,
+        height: 500,
+      },
+      {
+        name: 'small',
+        width: 600,
+      },
+      {
+        name: 'medium',
+        width: 900,
+      },
+      {
+        name: 'large',
+        width: 1400,
+      },
+      {
+        name: 'xlarge',
+        width: 1920,
+      },
+      {
+        name: 'og',
+        width: 1200,
+        height: 630,
+        crop: 'center',
+      },
+    ],
+    adminThumbnail: 'thumbnail',
     mimeTypes: ['image/*'],
   },
   fields: [
